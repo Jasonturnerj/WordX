a060ad8f745ae2f8e7315651fdde5dfaf31754a6
Jasonturnerj <jasonturner2350@gmail.com>
2024-06-26 17:14:36 -0700

    okokkk

--- db.js
+++ db.js
@@ -8,9 +8,6 @@ let db;
 if (process.env.NODE_ENV === "production") {
   db = new Client({
     connectionString: getDatabaseUri(),
-    ssl: {
-      rejectUnauthorized: false
-    }
   });
 } else {
   db = new Client({
